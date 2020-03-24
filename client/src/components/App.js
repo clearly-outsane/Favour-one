f7264d07e3daa3bbaf3f90d47ad61013d20821be
unknown <1starwarrior516@gmail.com>
2020-03-24 13:58:06 +0530

    First

---
+++ client/src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+// import * as actions from "../actions";
+
+const Landing = () => <h2>Landing</h2>;
+
+class App extends Component {
+  componentDidMount() {
+    // this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect()(App);
---
+++ client/src/setupProxy.js
@@ -0,0 +1,9 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function(app) {
+  app.use(
+    createProxyMiddleware(["/user"], {
+      target: "http://localhost:5000"
+    })
+  );
+};
